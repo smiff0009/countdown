524ad850c156b6a3c9826bec9f5872b5c8559b9c
smiff0009 <trevorgustaveson@gmail.com>
2025-01-05 15:39:59 -0700

    test

--- countdown.js
+++ countdown.js
@@ -1,9 +1,12 @@
+const output = document.querySelector('.output');
+output.textContent = "";
+
 function countdown() {
     for(let i = 10; i >= 0; i--) {
         const para = document.createElement('p');
-        const output = document.querySelector('.output');
-        if(i == 10) {
-            para.textContent = `Countdown ${i}`;
+        
+        if(i === 10) {
+            para.textContent = `Countdown \${i}`;
         }
         else if(i > 0){
             para.textContent = i;
