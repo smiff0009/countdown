3df9ef761393a4eb6bcecee5643a2a80a351bc86
smiff0009 <trevorgustaveson@gmail.com>
2025-01-05 15:30:51 -0700

    test

---
+++ countdown.js
@@ -0,0 +1,18 @@
+function countdown() {
+    for(let i = 10; i >= 0; i--) {
+        const para = document.createElement('p');
+        const output = document.querySelector('.output');
+        output.appendChild(para);
+        if(i == 10) {
+            para.textContent = `Countdown ${i}`;
+        }
+        else if(i > 0){
+            para.textContent = i;
+        }
+        else {
+            para.textContent = "Blast off!"
+        }
+    }
+}
+
+countdown();
