13fbcbe533739c5a73d0a27d052626187e32a19d
smiff0009 <trevorgustaveson@gmail.com>
2025-01-06 15:00:58 -0700

    test

--- countdown.js
+++ countdown.js
@@ -1,43 +1,4 @@
-/*let output = document.querySelector('.output');
-output.textContent = "";
-
-function countdown() {
-    for(let i = 10; i > 0; i--) {
-        const para = document.createElement('p');
-        
-        if(i === 10) {
-            para.textContent = `Countdown \${i}`;
-        }
-        else if(i === 0){
-            para.textContent = "Blast off!";
-        }
-        else {
-            para.textContent = i;
-        }
-        output.appendChild(para);
-    }
-}
-
-countdown();*/
-
-const output = document.getElementById("output");
-output.textContent = "";
-
-let i = 10;
-
-while (i >= 10) {
-    const para = document.createElement('p');
-    if (i === 10) {
-        para.textContent = `Countdown ${i}`;
-    }
-    else if (i === 0) {
-        para.textContent = 'Blast off!';
-    }
-    else {
-        para.textContent = i;
-    }
-    output.appendChild(para);
-
-    i--;
-};
-
+const body = document.body;
+const div = document.createElement("div");
+div.innerText = "Hello World"
+body.appendChild(div);
