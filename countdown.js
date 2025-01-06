932d60e2659640a147ff49a3d3cf2fad2bce3731
smiff0009 <trevorgustaveson@gmail.com>
2025-01-06 16:36:23 -0700

    test

--- countdown.js
+++ countdown.js
@@ -2,11 +2,13 @@
 const output = document.querySelector('.output');
 const para = document.createElement('p');
 const btn = document.querySelector('.click')
-
+/*
 function addText() {
     output.appendChild(para);
     para.textContent = "You clicked me!";
 }
-/*
-btn.addEventListener("click", addText());
-  */  
+*/
+btn.addEventListener("click", addText(){
+    output.appendChild(para);
+    para.textContent = "You clicked me!";
+});  
